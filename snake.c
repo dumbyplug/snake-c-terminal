057b3cace92676f94caf2e0884a68816e7f93fee
borday <iwasdumbyforu@gmail.com>
2025-04-08 22:20:21 +0400

    A different approach for random value generator

--- snake.c
+++ snake.c
@@ -34,7 +34,7 @@ void init_snake(int snake[], int *snake_size){
 	}
 }
 
-int random_food_appear(char grid[ROW][COL], int snake_size){
+int random_food_appear(char grid[ROW][COL], int snake[], int snake_size){
     /*
     This function generates a food on random place in the screen. and each 
 	time snake eats the food, it regenerates the food in another place and keeps score.
@@ -43,16 +43,71 @@ int random_food_appear(char grid[ROW][COL], int snake_size){
     Output:
         the position of the food
     */	
-	int random_row = 0, random_column = 0;
-	if(snake_size >= ROW * COL)
+	
+	
+	//int random_row, random_column;
+	if(snake_size >= (ROW - 2) * (COL - 2))
 		return -1;
-
-	while(grid[random_row][random_column] != ' '){
+	/*
+	do {
 		random_row = rand() % (ROW - 2) + 1;      
 		random_column = rand() % (COL - 2) + 1;
-	}
+	} while(grid[random_row][random_column] != ' ');
 
 	return random_row * 100 + random_column;
+	*/
+	int occupied = ROW * 2 + (COL - 2) * 2 + snake_size; 
+	int possible_places[ROW * COL];
+
+	// getting coordinates of all possible places
+	for(i = 0; i < ROW; i++){
+		for(j = 0; j < COL; j++){
+			// canceling the borders
+			if((grid[i][j] == 'E') || (grid[i][j] == 'H')) 
+				possible_places[i * COL + j] = -1;
+			else
+				possible_places[i * COL + j] = i * DIG + j;
+		}
+	}
+	// canceling the body of snake 
+	for(i = 0; i < snake_size; i++){
+		possible_places[(snake[i] / DIG) * COL + snake[i] % DIG] = -1;
+	}
+	
+	//endwin();
+
+	//for(i = 0; i < ROW * COL; i++){
+	//	printf("%d\n", possible_places[i]);
+	//}
+
+	char swapped = 0;
+	int temp;
+	for(i = 0; i < (ROW * COL) - 1; i++){
+		swapped = 0;
+		for(int j = 0; j < (ROW * COL) - i - 1; j++){
+			if(possible_places[j] > possible_places[j + 1]){
+				temp = possible_places[j];
+				possible_places[j] = possible_places[j + 1];
+				possible_places[j + 1] = temp;
+				swapped = 1;
+			}
+		}
+		if(!swapped)
+			break;
+	}
+	/*
+	printf("Testing 2\n");
+	for(i = 0; i < ROW * COL; i++){
+		printf("%d\n", possible_places[i]);
+	}
+	*/
+
+	
+	int random_number = rand() % (ROW * COL - occupied) + occupied;
+
+	//printf("%d\n", possible_places[random_number]);
+
+	return possible_places[random_number];
 }
 
 char collision_check(char grid[ROW][COL], int snake[]){
@@ -111,6 +166,8 @@ int main(void){
 	keypad(stdscr, TRUE);  
 	nodelay(stdscr, TRUE);
 
+	srand(time(NULL));
+
 	char grid[ROW][COL];
 	int snake[ROW * COL], snake_size, food = 516, grow = 0;
 	char snake_facing = '>';
@@ -156,12 +213,13 @@ int main(void){
 		grid[snake[i] / DIG][snake[i] % DIG] = '#';
 	} 
 	//Check the head before overwriting.
-	if (collision_check(grid, snake) == 2){
-		run = 0;
-	}
-	else if(collision_check(grid, snake) == 1){
-		food = random_food_appear(grid, snake_size);
-		grow = 1;
+	switch(collision_check(grid, snake)){
+		case 2:
+			run = 0;
+			break;
+		case 1:
+			food = random_food_appear(grid, snake, snake_size);
+			grow = 1;
 	}
 
 	grid[snake[0] / DIG][snake[0] % DIG] = '@';
