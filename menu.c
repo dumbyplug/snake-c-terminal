2a27edcf0cc69b3cd6ac1eec38fdccd83f88aa14
Tural Hajinabili <turalozel@gmail.com>
2025-04-17 08:34:50 +0400

    Menu is completed, ready to use

--- menu.c
+++ menu.c
@@ -3,34 +3,35 @@
 int main() {
 
 	short menu = 0;
-	short select = 1, subSelect = 1;
-	short limit = 3, subLimit = 1;
+	short select = 1, subSelect = 3;
+	short limit = 3, subLimit = 5;
 
-	short gridSize = 1, difficulty = 1;
+	short gridSize = 3, difficulty = 2, speed = 2;
 
 	char gridSizeText[11];
 	char difficultyText[11];
+	char speedText[11];
+	char message[50];
 
 	while (1) {
 		if (menu == 0) {
-			limit = 3;
 			printf("+---- Snake Game ----+\n");
 
 			switch (select) {
 			case 1:
 				printf("|      > Play <      |\tStarts the game with\n"
-						"|      Settings      |\tgiven settings...\n"
-						"|        Exit        |\n");
+					"|      Settings      |\tgiven settings...\n"
+					"|        Exit        |\n");
 				break;
 			case 2:
 				printf("|        Play        |\tConfigure settings\n"
-						"|    > Settings <    |\tfor the game...\n"
-						"|        Exit        |\n");
+					"|    > Settings <    |\tfor the game...\n"
+					"|        Exit        |\n");
 				break;
 			case 3:
 				printf("|        Play        |\tPlease don't click me...\n"
-						"|      Settings      |\n"
-						"|      > Exit <      |\n");
+					"|      Settings      |\n"
+					"|      > Exit <      |\n");
 				break;
 			default:
 				printf("|  An error occured  |\n");
@@ -40,48 +41,104 @@ int main() {
 			printf("+--------------------+\n");
 		}
 		else {
-			limit = 2;
+
+			if (select == 1)
+				gridSize = subSelect;
+			else if (select == 2)
+				difficulty = subSelect;
+			else
+				speed = subSelect;
+
 
 			if (difficulty == 1)
-				sprintf(difficultyText, "  #     >");
+				sprintf(difficultyText, "#    ");
 			else if (difficulty == 2)
-				sprintf(difficultyText, "< # #   >");
+				sprintf(difficultyText, "# #  ");
 			else
-				sprintf(difficultyText, "< # # #  ");
+				sprintf(difficultyText, "# # #");
+
+			if (speed == 1)
+				sprintf(speedText, "#    ");
+			else if (speed == 2)
+				sprintf(speedText, "# #  ");
+			else
+				sprintf(speedText, "# # #");
 
 			switch (gridSize) {
 			case 1:
-				sprintf(gridSizeText, "  10x10 >");
+				sprintf(gridSizeText, "10x10");
 				break;
 			case 2:
-				sprintf(gridSizeText, "< 15x15 >");
+				sprintf(gridSizeText, "15x15");
 				break;
 			case 3:
-				sprintf(gridSizeText, "< 20x20 >");
+				sprintf(gridSizeText, "20x20");
 				break;
 			case 4:
-				sprintf(gridSizeText, "< 25x25 >");
+				sprintf(gridSizeText, "25x25");
 				break;
 			case 5:
-				sprintf(gridSizeText, "< 30x30  ");
+				sprintf(gridSizeText, "30x30");
 				break;
 			}
 
 			printf("+----- Configuration -----+\n");
 
-			switch (select)
-			{
-			case 1:
-				subLimit = 5;
-				break;
-			case 2:
-				subLimit = 3;
-				break;
-			default:
-				break;
-			}
 
-			printf("|   Grid size   %s |\n|  Difficulty   %s |\n",gridSizeText,difficultyText);
+
+			printf("|   Grid size   ");
+
+			if (select == 1 && subSelect != 1)
+				printf("< ");
+			else
+				printf("  ");
+
+			printf("%s", gridSizeText);
+
+			if (select == 1 && subSelect != subLimit)
+				printf(" >");
+			else
+				printf("  ");
+
+			printf(" |\n");
+
+
+
+			printf("|  Difficulty   ");
+
+			if (select == 2 && subSelect != 1)
+				printf("< ");
+			else
+				printf("  ");
+
+			printf("%s", difficultyText);
+
+			if (select == 2 && subSelect != subLimit)
+				printf(" >");
+			else
+				printf("  ");
+
+			printf(" |\n");
+
+
+
+			printf("|       Speed   ");
+
+			if (select == 3 && subSelect != 1)
+				printf("< ");
+			else
+				printf("  ");
+
+			printf("%s", speedText);
+
+			if (select == 3 && subSelect != subLimit)
+				printf(" >");
+			else
+				printf("  ");
+
+			printf(" |\n");
+
+
 
 			printf("+-------------------------+\n");
 		}
@@ -90,16 +147,32 @@ int main() {
 		{
 		case 'w':
 			select--;
+			if (menu == 1) {
+				if (select == 1)
+					subSelect = gridSize;
+				else if (select == 2)
+					subSelect = difficulty;
+				else
+					subSelect = speed;
+			}
 			break;
 		case 's':
 			select++;
+			if (menu == 1) {
+				if (select == 1)
+					subSelect = gridSize;
+				else if (select == 2)
+					subSelect = difficulty;
+				else
+					subSelect = speed;
+			}
 			break;
 		case 'a':
-			if (menu != 0)
+			if (menu == 1)
 				subSelect--;
 			break;
 		case 'd':
-			if (menu != 0)
+			if (menu == 1)
 				subSelect++;
 			break;
 		case ' ':
@@ -119,6 +192,10 @@ int main() {
 					break;
 				case 2:
 					menu = 1;
+					select = 1;
+					limit = 3;
+					subLimit = 5;
+					subSelect = gridSize;
 					break;
 				case 3:
 					exit();
@@ -126,15 +203,45 @@ int main() {
 				}
 			}
 			else {
-				// later
+				menu = 0;
+				select = 1;
+				limit = 3;
+				///////////////////////////////
+				///// config function here
+				///////////////////////////////
 			}
 			break;
 		}
 
-		if (select > limit)
+		if (select > limit) {
 			select = 1;
-		else if (select <= 0)
-			select = 3;
+			if (menu == 1) {
+				subSelect = gridSize;
+			}
+		}
+		else if (select <= 0) {
+			select = limit;
+			if (menu == 1) {
+				subSelect = speed;
+			}
+		}
+
+
+		switch (select)
+		{
+		case 1:
+			subLimit = 5;
+			break;
+		case 2:
+			subLimit = 3;
+			break;
+		case 3:
+			subLimit = 3;
+			break;
+		default:
+			subLimit = 5;
+		}
+
 
 		if (subSelect > subLimit)
 			subSelect = subLimit;
